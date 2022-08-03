ef7b1b53191a3d20b0b04263225d1f5403856e31
melgeller <melgeller.foto@gmail.com>
2022-08-03 10:00:17 +0200

    final

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,7 +10,8 @@ const About = () => {
         </div>
         <div className='textoAbout'>
           <h2>Hi!</h2>
-          <h3>My name is Silvia Abad</h3>
+          <h3>My name is Silvia</h3>
+          <p>and Mel Geller is my nickname 💙</p>
           <h4>English</h4>
           <p>
             I was born in 1990 in Murcia, Spain. I was always very passionate
@@ -19,8 +20,7 @@ const About = () => {
             world as a seller. I also participate in a gaming podcast called
             Stay/Awake and have streamed on Twitch. I'm a very curious person, I
             like to learn and above all to be able to perform in a creative
-            position that allows me to continue growing. If you want to know
-            more details about me, download my CV
+            position that allows me to continue growing. 
           </p>
           <h4>Español</h4>
           <p>
@@ -29,10 +29,9 @@ const About = () => {
             ordenadores, el arte y los videojuegos. Estudié fotografía en la
             EASD de Murcia en 2011 y desde entonces me he desempeñado en el
             mundo textil como vendedora. También participo en un podcast de
-            videojuegos llamado SA y he transmitido en Twitch. Soy una persona
+            videojuegos llamado Stay/Awake y he transmitido en Twitch. Soy una persona
             muy curiosa, me gusta aprender y sobre todo poder desempeñarme en un
-            puesto creativo que me permita seguir creciendo. Si quieres saber
-            más detalles sobre mí, descarga mi CV
+            puesto creativo que me permita seguir creciendo. 
           </p>
         </div>
       </div>
