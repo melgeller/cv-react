523af238d842034ac72b1d6907b2f010c465cd52
melgeller <melgeller.foto@gmail.com>
2022-08-29 11:41:42 +0200

    galeria coding

--- src/pages/Coding.jsx
+++ src/pages/Coding.jsx
@@ -6,26 +6,29 @@ const Coding = () => {
     <div className='codingGallery'>
     <div className='codingCards'>
     <div className='cardsC'>
-    <img src="../../assets/ff.png" alt=""/>
+    <a href="https://cv-react-silvia.vercel.app/assets/ff.png"><img src="../../assets/ff.png" alt=""/></a>
     <h2>Final Fantasy Fanpage</h2>
     <h3>Created with HTML and CSS</h3>
     <div className='textoCoding'>
         <a href="https://github.com/melgeller/final-fantasy-page"><img src="../assets/git.png" alt=""/></a>
         <a href="https://www.linkedin.com/posts/melgeller-foto_hola-de-nuevo-el-tercer-d%C3%ADa-de-clase-activity-6930947779242418176-rvZX?utm_source=linkedin_share&utm_medium=member_desktop_web"><img src="../assets/linkedin.png" alt=""/></a>
-        <a href="https://melgeller.github.io/final-fantasy-page/"><img className='unico' src="../assets/link.png" alt=""/></a>
+        <a href="https://melgeller.github.io/final-fantasy-page/">
+        <img className='unico' src="../assets/link.png" alt=""/></a>
     </div>
     </div>
     <div className='cardsC'>
-    <img src="../../assets/store.png" alt=""/>
+    <a href="https://cv-react-silvia.vercel.app/assets/store.png"><img src="../../assets/store.png" alt=""/></a>
     <h2>Shop Online</h2>
     <h3>Created with HTML and CSS</h3>
     <div className='textoCoding'>
         <a href="https://github.com/melgeller/shop-css"><img src="../assets/git.png" alt=""/></a>
-        <a href="https://www.linkedin.com/posts/melgeller-foto_hola-acabamos-de-comenzar-el-bootcamp-activity-6929850792514367488-86Tg?utm_source=linkedin_share&utm_medium=member_desktop_web"><img src="../assets/linkedin.png" alt=""/></a>
+        <a href="https://www.linkedin.com/posts/melgeller-foto_hola-acabamos-de-comenzar-el-bootcamp-activity-6929850792514367488-86Tg?utm_source=linkedin_share&utm_medium=member_desktop_web">
+        <img src="../assets/linkedin.png" alt=""/></a>
     </div>
     </div>
     <div className='cardsC'>
-    <img src="../../assets/pokeapi.png" alt=""/>
+    <a href="https://cv-react-silvia.vercel.app/assets/pokeapi.png"><img src="../../assets/pokeapi.png" alt=""/></a>
+    
     <h2>PokeAPI</h2>
     <h3>Created with JavaScript, HTML and SCSS.</h3>
     <h3>From <a href="https://pokeapi.co/">PokeApi</a></h3>
@@ -37,7 +40,7 @@ const Coding = () => {
 
     </div>
     <div className='cardsC'>
-    <img src="../../assets/net.png" alt=""/>
+    <a href="https://cv-react-silvia.vercel.app/assets/net.png"><img src="../../assets/net.png" alt=""/></a>
     <h2>Netflix</h2>
     <h3>Created with Angular, HTML, JavaScript and SCSS</h3>
     <div className='textoCoding'>
@@ -47,7 +50,8 @@ const Coding = () => {
 
     </div>
     <div className='cardsC'>
-    <img src="../../assets/animal.png" alt=""/>
+    <a href="https://cv-react-silvia.vercel.app/assets/animal.png"><img src="../../assets/animal.png" alt=""/></a>
+    
     <h2>Animal Crossing Fanpage</h2>
     <h3>Created with Angular, JavaScript and SCSS</h3>
     <div className='textoCoding'>
@@ -57,7 +61,8 @@ const Coding = () => {
 
     </div>
     <div className='cardsC'>
-    <img src="../../assets/ghibli.png" alt=""/>
+    <a href="https://cv-react-silvia.vercel.app/assets/ghibli.png"><img src="../../assets/ghibli.png" alt=""/></a>
+    
     <h2>Studio Ghibli API</h2>
     <h3>Created with Angular, JavaScript and SCSS</h3>
     <h3>From <a href="https://ghibliapi.herokuapp.com/">GhibliApi</a></h3>
@@ -69,7 +74,8 @@ const Coding = () => {
 
     </div>
     <div className='cardsC'>
-    <img src="../../assets/rick.png" alt=""/>
+    <a href="https://cv-react-silvia.vercel.app/assets/rick.png"><img src="../../assets/rick.png" alt=""/></a>
+    
     <h2>Rick and Morty API</h2>
     <h3>Created with Authorization, React, JavaScript and SCSS</h3>
     <h3>From <a href="https://rickandmortyapi.com/">Rick and Morty Api</a></h3>
@@ -80,7 +86,8 @@ const Coding = () => {
 
     </div>
     <div className='cardsC'>
-    <img src="../../assets/sh.png" alt=""/>
+    <a href="https://cv-react-silvia.vercel.app/assets/sh.png"><img src="../../assets/sh.png" alt=""/></a>
+    
     <h2>Silent Hill API</h2>
     <h3>Created with Authorization, React, NodeJS, MongoDB, JavaScript and SCSS</h3>
     <h3>From <a href="https://node-silent-hill.vercel.app/games">Silent Hill</a> api, made using MongoDB and NodeJS.</h3>
