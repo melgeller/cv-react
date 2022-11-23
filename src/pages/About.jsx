dc57c4836e650d9ddbc8fb39045280e0b7491511
melgeller <melgeller.foto@gmail.com>
2022-11-23 13:53:02 +0100

    about iconos

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <div className='todoAbout'>
       <div className='form'>
         <div className='foto'>
-          <img src='../../assets/imgport.jpg' alt='' />
+          <img className="profilePic" src='../../assets/imgport.jpg' alt='' />
         </div>
         <div className='textoAbout'>
           <div className='textoIdiomas'>
@@ -27,45 +27,37 @@ const About = () => {
           </div>
           <div className='paragraph'>
             <p>
-              I was born in 1990 in <span className='melgeller'>Murcia</span>, Spain. I was always very passionate
+              I was born in 1990 in <span className='melgeller'>Murcia</span>, Spain. I have always been very passionate
               about computers, art and video games. I studied photography at the
               <span className='melgeller'> EASD</span> (escuela de arte y superior de diseño) in Murcia in 2011 and since then I have worked in the textile
               world as a seller. I also participate in a gaming podcast called
               <span className='melgeller'> Stay/Awake</span> and have streamed on Twitch. I'm a very curious person,
-              I like to learn and above all to be able to perform in a creative
+              I like to learn and specially to be able to perform in a creative
               position that allows me to continue growing.
             </p>
           </div>
 
           <div className='paragraph'>
             <p className='icons'>🔧</p>
-            <p>
-              Adobe Photoshop
-            </p>
-            <p>
-              Adobe Lightroom
-            </p>
-            <p>
-              HTML
-            </p>
-            <p>
-              CSS and SCSS
-            </p>
-            <p>
-              MongoDB
-            </p>
-            <p>
-              JavaScript
-            </p>
-            <p>
-              React
-            </p>
-            <p>
-              Angular
-            </p>
-            <p>
-              NODEJS
-            </p>
+            <div className="iconos">
+            <img src="../../assets/iconos/photoshop.png" alt=""/>
+            <img src="../../assets/iconos/lightroom.png" alt=""/>
+            <img src="../../assets/iconos/js.png" alt=""/>
+            <img src="../../assets/iconos/html.png" alt=""/>
+            <img src="../../assets/iconos/css.png" alt=""/>
+            <img src="../../assets/iconos/sass.png" alt=""/>
+            <img src="../../assets/iconos/mongo.png" alt=""/>
+            <img src="../../assets/iconos/react.png" alt=""/>
+            <img src="../../assets/iconos/angular.png" alt=""/>
+            <img src="../../assets/iconos/node.png" alt=""/>
+            <img src="../../assets/iconos/insomnia.png" alt=""/>
+            <img src="../../assets/iconos/obs.png" alt=""/>
+            <img src="../../assets/iconos/twitch.png" alt=""/>
+            <img src="../../assets/iconos/figurama.png" alt=""/>
+            <img src="../../assets/iconos/campaign.png" alt=""/>
+            <img src="../../assets/iconos/target.jpg" alt=""/>
+            <img src="../../assets/iconos/aa.png" alt=""/>
+            </div>
           </div>
         </div>
       </div>
